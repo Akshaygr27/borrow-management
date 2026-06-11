6a5a2785b0b0d9c2ce16de606b78e0909f009510
Akshaygr27 <akshayganesh656@gmail.com>
2026-06-12 04:42:34 +0530

    add MongoDB connection and update package.json dependencies(nodemon)

--- borrow-management-backend/app.js
+++ borrow-management-backend/app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
+const db = require('./config/db')
+
+//mongoDB Connection
+db();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
